4efe511d73ebd3b368c996aee432fb054e7107d7
sxlwrl <Vadik120404@>
2023-04-18 15:21:33 +0300

    some changes have been added

--- src/script_welcome.js
+++ src/script_welcome.js
@@ -13,19 +13,6 @@ const passwordValue = document.querySelector('#password');
 const loginSection = document.querySelector('.section--login');
 const regSection = document.querySelector('.section--register');
 
-const temp_acc = {
-    login: 'test00',
-    password: '123123',
-};
-
-loginButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (loginValue.value === temp_acc.login && passwordValue.value === temp_acc.password) {
-        window.location.href = 'main.html';
-    }
-
-})
-
 btn_sign.forEach(item => item.addEventListener('click', function() {
     loginSection.classList.toggle('hidden');
     regSection.classList.toggle('hidden');
