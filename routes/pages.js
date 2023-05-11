58b817e9751d622a1243ae7345e14b358356ced3
sxlwrl <Vadik120404@>
2023-05-11 20:26:41 +0300

    The implementation of an automatic redirect from the login and register pages to main has been added if the user is already logged in

--- routes/pages.js
+++ routes/pages.js
@@ -2,17 +2,19 @@
 
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/users');
 
-router.get("/", (req, res) => {
-    res.render('login');
+router.get(['/', '/login'], userController.isLoggedIn, (req, res) => {
+    req.user ? res.redirect('/main') : res.render('login');
 });
 
-router.get("/register", (req, res) => {
-    res.render('register');
+router.get('/register', userController.isLoggedIn, (req, res) => {
+    req.user ? res.redirect('/main') : res.render('register');
 });
 
-router.get("/main", (req, res) => {
-    res.render('main');
+router.get('/main', userController.isLoggedIn, (req, res) => {
+    console.log(req.user);
+    req.user ? res.render('main', {user: req.user}) : res.redirect('/');
 });
 
 module.exports = router;
