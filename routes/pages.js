5335cf184b9d4221b701a14216c9ccbaceda4474
sxlwrl <Vadik120404@>
2023-05-20 22:18:49 +0300

    Middleware and taskModel have been included. Some changes in routers. When user logs in, all tasks are loaded for him

--- routes/pages.js
+++ routes/pages.js
@@ -3,18 +3,20 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
+const taskModel = require("../models/task-model");
+const {authMiddleware} = require('../middlewares/authMiddlewares');
 
-router.get(['/', '/login'], userController.isLoggedIn, (req, res) => {
+router.get(['/', '/login'], (req, res) => {
     req.user ? res.redirect('/main') : res.render('login');
 });
 
-router.get('/register', userController.isLoggedIn, (req, res) => {
+router.get('/register', (req, res) => {
     req.user ? res.redirect('/main') : res.render('register');
 });
 
-router.get('/main', userController.isLoggedIn, (req, res) => {
-    console.log(req.user);
-    req.user ? res.render('main', {user: req.user}) : res.redirect('/');
+router.get('/main', authMiddleware, async (req, res) => {
+    const tasks = await taskModel.getTasksByUserId(req.user.ID);
+    req.user ? res.render('main', {user: req.user, tasks}) : res.redirect('/');
 });
 
 module.exports = router;
