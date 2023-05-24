e457b9a0bdc421da0fdb487711f933e6de793db4
Alina <91843875+aljolen@users.noreply.github.com>
2023-05-24 22:01:23 +0200

    app.js fix

--- src/app.js
+++ src/app.js
@@ -1,42 +1,44 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const hbs = require('hbs');
-const cookieParser = require('cookie-parser');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const hbs = require("hbs");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 
 dotenv.config({
-    path: './src/.env',
+  path: "./src/.env",
 });
 
-const {database} = require('../database/database');
-
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // handlebars
 
-const location = path.join(__dirname, '../public');
+const location = path.join(__dirname, "../public");
 
 app.use(express.static(location));
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
-const partialsPath = path.join(__dirname, '../views/partials');
+const partialsPath = path.join(__dirname, "../views/partials");
 hbs.registerPartials(partialsPath);
 
-app.use('/', require('../routes/pages'));
-app.use('/auth', require('../routes/auth'));
-app.use('/main', require('../routes/task-router'));
+hbs.registerHelper("formatDate", function (dateString) {
+  const options = { weekday: "short", month: "short", day: "2-digit" };
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", options);
+});
+
+app.use("/", require("../routes/pages"));
+app.use("/auth", require("../routes/auth"));
+app.use("/main", require("../routes/task-router"));
 
 // app listen
 
 app.listen(PORT, () => {
-    console.log(`Server has been started @ Port ${PORT}`);
+  console.log(`Server has been started @ Port ${PORT}`);
 });
