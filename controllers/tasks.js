74fe29d5636d6f4bfabc31dabfd07898264f45e7
Alina <91843875+aljolen@users.noreply.github.com>
2023-05-24 21:57:57 +0200

    fix js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,63 +1,61 @@
-'use strict';
+"use strict";
 
-const taskModel = require('../models/task-model');
+const taskModel = require("../models/task-model");
 
 exports.createTask = async (req, res) => {
-    try {
-        const taskData = {
-            user_id: req.user.ID,
-            task_name: req.body.name,
-            task_description: req.body.description,
-            task_priority: req.body.priority,
-            task_date: req.body.date,
-            task_status: req.body.status,
-        };
+  try {
+    const taskData = {
+      user_id: req.user.ID,
+      task_name: req.body.name,
+      task_description: req.body.description,
+      task_priority: req.body.priority,
+      task_date: req.body.date,
+      task_status: 1,
+    };
 
-        await taskModel.createTask(taskData);
-        res.redirect('/main');
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
-    }
+    await taskModel.createTask(taskData);
+    res.redirect("/main");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
 };
 
 exports.editTask = async (req, res) => {
-    try {
-        const tasks = await taskModel.getTasksByUserId(req.user.ID);
-        res.render(`main`, {tasks, taskID: req.params.id, editing: true});
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
-    }
-}
+  try {
+    const tasks = await taskModel.getTasksByUserId(req.user.ID);
+    res.render(`main`, { tasks, taskID: req.params.id, editing: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
+};
 
 exports.updateTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = {
-            task_name: req.body.name,
-            task_description: req.body.description,
-            task_priority: req.body.priority,
-            task_status: req.body.status,
-        };
+  try {
+    const taskId = req.params.id;
+    const taskData = {
+      task_name: req.body.name,
+      task_description: req.body.description,
+      task_priority: req.body.priority,
+      task_status: req.body.status,
+    };
 
-        const updated = await taskModel.updateTask(taskId, taskData);
-        updated ? res.redirect('/main') : res.status(404).send('Task not found');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
+    const updated = await taskModel.updateTask(taskId, taskData);
+    updated ? res.redirect("/main") : res.status(404).send("Task not found");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 exports.deleteTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const deleted = await taskModel.deleteTask(taskId);
-        deleted ? res.redirect('/main') : res.status(404).send('Task not found');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const taskId = req.params.id;
+    const deleted = await taskModel.deleteTask(taskId);
+    deleted ? res.redirect("/main") : res.status(404).send("Task not found");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
