351d4d8266de81a9b032c82442330b59f6e3eb2b
sxlwrl <Vadik120404@>
2023-05-28 17:38:18 +0300

    In this change has been added class RenderText in which we have 2 methods for rendering errors or success messages. All user's functions have been implemented in class

--- controllers/users.js
+++ controllers/users.js
@@ -1,83 +1,97 @@
 'use strict';
 
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const {promisify} = require('util');
 const userModel= require('../models/user-model');
 
 const dayToMilliseconds = (day) => (day * 24 * 60 * 60 * 1000);
 
-exports.login = async (req, res) => {
-    try {
-        const {data, password} = req.body;
-        if (!data || !password) {
-            return res.status(400).render('login', {msg: `There is an empty field`, msg_type: 'error'});
-        }
+class RenderText {
+    constructor(res, status, page, errorMessage) {
+        this.res = res;
+        this.status = status;
+        this.page = page;
+        this.errorMessage = errorMessage;
+    };
 
-        const user = await userModel.getUser(data);
+    renderError = () => {
+        return this.res.status(this.status).render(this.page, { msg: this.errorMessage, msg_type: 'error' });
+    };
 
-        if (!user || !(await userModel.comparePassword(password, user.PASS))) {
-            return res.status(401).render('login', {msg: `Email or password isn't correct`, msg_type: 'error'});
-        }
-
-        const token = await userModel.generateToken(user.ID);
-
-        const cookieOptions = {
-            expires: new Date(Date.now() + dayToMilliseconds(process.env.JWT_COOKIE_EXPIRES)),
-            httpOnly: true,
-        }
-
-        res.cookie(`account`, token, cookieOptions);
-        res.status(200).redirect('/main');
-
-    }
-    catch (error) {
-        console.error(error);
-    }
-};
-
-exports.register = async (req, res) => {
-    const {username, email, password, confirm_password} = req.body;
-    console.log(req.body);
-
-    if (!username || !email || !password || !confirm_password) {
-        let errorMessage = '';
-
-        (!username || !email) ? errorMessage = `Username or email is not valid` : errorMessage = `Password not entered`;
-
-        return res.status(400).render('register', { msg: errorMessage, msg_type: 'error' });
-    }
-
-    if (password !== confirm_password) {
-        return res.status(400).render('register', { msg: `Password isn't correct`, msg_type: 'error' });
-    }
-
-    try {
-        const result = await userModel.checkExistingUser(username, email);
-
-        if (result.isExistingUsername) {
-            return res.status(400).render('register', { msg: `Username already taken`, msg_type: 'error' });
-        }
-
-        if (result.isExistingEmail) {
-            return res.status(400).render('register', { msg: `Email id already taken`, msg_type: 'error' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await userModel.registerUser(username, email, hashedPassword);
-        return res.render('register', { msg: 'User Registration Success', msg_type: 'success'});
-    }
-    catch (error) {
-        console.error(error);
-    }
-
-};
-
-exports.logout = async (req, res) => {
-    res.cookie('account', 'logout', {
-        expires: new Date(Date.now() + 2 * 1000),
-        httpOnly: true,
-    });
-    res.status(200).redirect('/');
+    renderSuccess = () => {
+        return this.res.status(this.status).render(this.page, { msg: this.errorMessage, msg_type: 'success' });
+    };
 }
+
+class User {
+    async login(req, res) {
+        const {data, password} = req.body;
+
+        if (!data || !password) {
+            return new RenderText(res, 400, 'login', `There is an empty field`).renderError();
+        }
+
+        try {
+            const user = await userModel.getUser(data);
+
+            if (!user || !(await userModel.comparePassword(password, user?.PASS))) {
+                return new RenderText(res, 401, 'login', `Email or password isn't correct`).renderError();
+            }
+
+            const token = await userModel.generateToken(user.ID);
+
+            const cookieOptions = {
+                expires: new Date(Date.now() + dayToMilliseconds(process.env.JWT_COOKIE_EXPIRES)),
+                httpOnly: true,
+            }
+
+            res.cookie(`account`, token, cookieOptions);
+            res.status(200).redirect('/main');
+
+        }
+        catch (error) {
+            console.error(error);
+        }
+    };
+
+    async register(req, res) {
+        const {username, email, password, confirm_password} = req.body;
+
+        const errorMessage = (!username || !email) ? `Username or email is not valid` :
+                (!password || !confirm_password) ? `Password is not entered` :
+                (password !== confirm_password) ? `Password isn't correct` : ``;
+
+        if (errorMessage) {
+            return new RenderText(res, 400, 'register', errorMessage).renderError();
+        }
+
+        try {
+            const result = await userModel.checkExistingUser(username, email);
+
+            const errorMessage = result.isExistingUsername ? `Username is already taken` :
+                result.isExistingEmail ? `Email is already taken` : '';
+
+            if (errorMessage) {
+                return new RenderText(res, 400, 'register', errorMessage).renderError();
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            await userModel.registerUser(username, email, hashedPassword);
+            return new RenderText(res, 200, 'register', `User registration is successful`).renderSuccess();
+        }
+        catch (error) {
+            console.error(error.message);
+        }
+
+    };
+
+    async logout(req, res) {
+        res.cookie('account', 'logout', {
+            expires: new Date(Date.now() + 2 * 1000),
+            httpOnly: true,
+        });
+        res.status(200).redirect('/');
+    };
+}
+
+module.exports = new User();
