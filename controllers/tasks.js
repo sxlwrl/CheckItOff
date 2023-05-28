6e91bad587a2cff95fcbc8ffd22fb4aae4fbd0f4
sxlwrl <Vadik120404@>
2023-05-28 17:04:40 +0300

    task model has been redesigned and implemented using the class. getCurrentDate function has been created as well

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,67 +2,78 @@
 
 const taskModel = require('../models/task-model');
 
-exports.createTask = async (req, res) => {
-    try {
-        const taskData = {
-            user_id: req.user.ID,
-            task_name: req.body.name,
-            task_description: req.body.description,
-            task_priority: req.body.priority,
-            task_date: req.body.date,
-            task_status: 1,
-        };
+function getCurrentDate() {
+    const currentDate = new Date();
 
-        // if (new Date(taskData.task_date).getDate() < 25) {
-        //     console.log('5');
-        // }
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
 
-        await taskModel.createTask(taskData);
-        res.redirect('/main');
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
-    }
-};
-
-exports.editTask = async (req, res) => {
-    try {
-        const tasks = await taskModel.getTasksByUserId(req.user.ID);
-        const task = await taskModel.getTaskById(req.params.id);
-        res.render(`main`, {tasks, task, taskID: req.params.id, editing: true});
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
-    }
+    return `${year}-${month}-${day}`;
 }
 
-exports.updateTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = {
-            task_name: req.body.name,
-            task_description: req.body.description,
-            task_priority: req.body.priority,
-            task_date: req.body.date,
-        };
+class Task {
+    async createTask(req, res) {
+        try {
+            const taskData = {
+                user_id: req.user.ID,
+                task_name: req.body.name,
+                task_description: req.body.description,
+                task_priority: req.body.priority,
+                task_date: req.body.date,
+                task_status: 1,
+            };
 
-        const updated = await taskModel.updateTask(taskId, taskData);
-        updated ? res.redirect('/main') : res.status(404).send('Task not found');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-};
+            await taskModel.createTask(taskData);
+            res.redirect('/main');
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).send('Error');
+        }
+    };
 
-exports.deleteTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const deleted = await taskModel.deleteTask(taskId);
-        deleted ? res.redirect('/main') : res.status(404).send('Task not found');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-};
+    async editTask(req, res) {
+        try {
+            const tasks = await taskModel.getTasksByUserId(req.user.ID);
+            const task = await taskModel.getTaskById(req.params.id);
+            const date = getCurrentDate();
+            res.render(`main`, {tasks, task, taskID: req.params.id, editing: true, date});
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).send('Error');
+        }
+    };
+
+    async updateTask(req, res) {
+        try {
+            const taskId = req.params.id;
+            const taskData = {
+                task_name: req.body.name,
+                task_description: req.body.description,
+                task_priority: req.body.priority,
+                task_date: req.body.date,
+            };
+
+            const updated = await taskModel.updateTask(taskId, taskData);
+            updated ? res.redirect('/main') : res.status(404).send('Task not found');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
+    };
+
+    async deleteTask(req, res) {
+        try {
+            const deleted = await taskModel.deleteTask(req.params.id);
+            deleted ? res.redirect('/main') : res.status(404).send('Task not found');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
+    };
+}
+
+module.exports = new Task();
+
