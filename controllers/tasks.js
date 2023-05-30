a938c7c76932952ad3bfd96187e823c0df35b98e
sxlwrl <Vadik120404@>
2023-05-30 16:11:49 +0300

    added check for empty fields

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,6 +5,14 @@ const taskModel = require('../models/task-model');
 class Task {
     async createTask(req, res) {
         try {
+
+            if (!req.body.name || !req.body.date) {
+                const tasks = await taskModel.getTasksByUserId(req.user.ID);
+                const user = req.user;
+                const error = 'Invalid data to create a task';
+                return res.render('main', { tasks, user, error });
+            }
+
             const taskData = {
                 user_id: req.user.ID,
                 task_name: req.body.name,
@@ -13,7 +21,7 @@ class Task {
                 task_date: req.body.date,
                 task_status: 1,
             };
-            console.log(req.body);
+
             await taskModel.createTask(taskData);
             res.redirect('/main');
         } catch (error) {
@@ -34,7 +42,6 @@ class Task {
                 editing: true,
                 user,
             });
-            const x = 'x';
         } catch (error) {
             console.error(error);
             res.status(500).send('Error');
@@ -44,6 +51,14 @@ class Task {
     async updateTask(req, res) {
         try {
             const taskId = req.params.id;
+            if (!req.body.name || !req.body.date) {
+                const tasks = await taskModel.getTasksByUserId(req.user.ID);
+                const task = await taskModel.getTaskById(taskId);
+                const user = req.user;
+                const error = 'Invalid data to edit a task';
+                return res.render('main', { tasks, task, user, error });
+            }
+
             const taskData = {
                 task_name: req.body.name,
                 task_description: req.body.description,
