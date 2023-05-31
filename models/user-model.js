63a052c404c4a75c2602deff84e9bf4bdef70951
sxlwrl <Vadik120404@>
2023-05-31 17:12:13 +0300

    some changes

--- models/user-model.js
+++ models/user-model.js
@@ -1,50 +1,65 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {database} = require('../database/database');
+const { database } = require('../database/database');
 
 class UserModel {
     constructor(database) {
         this.database = database;
     }
 
-    async comparePassword(password, hash){
+    async comparePassword(password, hash) {
         return await bcrypt.compare(password, hash);
     }
 
-    async generateToken(id){
-        return jwt.sign({id: id}, process.env.JWT_SECRET, {
+    async generateToken(id) {
+        return jwt.sign({ id: id }, process.env.JWT_SECRET, {
             expiresIn: process.env.JWT_EXPIRES_IN,
         });
-    };
+    }
 
-    async getUser(data){
+    async getUser(data) {
         return new Promise((resolve, reject) => {
-            this.database.query('SELECT * FROM users WHERE username=? OR email=?', [data, data], (error, result) => {
-                error ? reject(error) : resolve(result[0]);
-            });
+            this.database.query(
+                'SELECT * FROM users WHERE username=? OR email=?',
+                [data, data],
+                (error, result) => {
+                    error ? reject(error) : resolve(result[0]);
+                }
+            );
         });
-    };
+    }
 
     checkExistingUser = (username, email) => {
         return new Promise((resolve, reject) => {
-            this.database.query('SELECT username, email FROM users WHERE username=? OR email=?', [username, email], (error, result) => {
-                if (error) {
-                    reject(error);
+            this.database.query(
+                'SELECT username, email FROM users WHERE username=? OR email=?',
+                [username, email],
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        const isExistingUsername = result.find(
+                            (element) => element.username === username
+                        );
+                        const isExistingEmail = result.find(
+                            (element) => element.email === email
+                        );
+                        resolve({ isExistingUsername, isExistingEmail });
+                    }
                 }
-                else {
-                    const isExistingUsername = result.find(element => element.username === username);
-                    const isExistingEmail = result.find(element => element.email === email);
-                    resolve({ isExistingUsername, isExistingEmail });
-                }
-            });
+            );
         });
     };
 
     registerUser = (username, email, hashedPassword) => {
         return new Promise((resolve, reject) => {
-            this.database.query('INSERT INTO users SET ?', {username: username, email: email, pass: hashedPassword}, (error, result) => {
-                error ? reject(error) : resolve(result);
-            });
+            this.database.query(
+                'INSERT INTO users SET ?',
+                { username: username, email: email, pass: hashedPassword },
+                (error, result) => {
+                    error ? reject(error) : resolve(result);
+                }
+            );
         });
     };
 }
--- routes/task-router.js
+++ routes/task-router.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/task', authMiddleware, taskController.createTask);
 router.get('/task/:id/edit', authMiddleware, taskController.editTask);
 router.post('/task/:id', authMiddleware, taskController.updateTask);
-router.post('/task/:id/delete', taskController.deleteTask);
+router.post('/task/:id/delete', authMiddleware, taskController.deleteTask);
 router.post('/task/:id/status', authMiddleware, taskController.updateTaskStatus);
 
 module.exports = router;
